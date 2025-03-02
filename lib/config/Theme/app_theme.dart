9f7163a624465d2016af2836fe64269bd696b5e1
TheLogan21 <miguel_dmartillo@outlook.com>
2025-03-01 23:46:24 -0500

    thema hecho

---
+++ lib/config/Theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const List<Color> coloresTemas = [
+  Colors.blue,
+  Colors.red,
+  Colors.deepPurple,
+  Colors.yellow,
+  Colors.brown,
+  Colors.deepOrange,
+  Colors.teal,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, "Selected Color must be grater than 0"),
+        assert(selectedColor < coloresTemas.length,
+            "Selected Color must be less or equal to ${coloresTemas.length - 1} ");
+
+  ThemeData getTheme() =>
+      ThemeData(colorSchemeSeed: coloresTemas[selectedColor]);
+}
