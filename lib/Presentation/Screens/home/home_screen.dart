2f7f1211b8ffe0b862571b157454d8d2bdc3ef61
TheLogan21 <miguel_dmartillo@outlook.com>
2025-03-02 00:44:17 -0500

    ListView and ListTile terminados

--- lib/Presentation/Screens/home/home_screen.dart
+++ lib/Presentation/Screens/home/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,8 +9,52 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter + Material"),
-        
-      ) ,
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuDeItems = appMenuItems[index];
+        return _CustomListTile(menuDeItems: menuDeItems);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuDeItems,
+  });
+
+  final MenuItems menuDeItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(
+        menuDeItems.icono,
+        color: colors.primary,
+      ),
+      title: Text(menuDeItems.title),
+      subtitle: Text(menuDeItems.subtitle),
+      trailing: Icon(
+        Icons.arrow_forward_ios_sharp,
+        color: colors.primary,
+      ),
+      onTap: () {
+        //TODO: navegar a otra pagina-pantalla
+      },
     );
   }
 }
