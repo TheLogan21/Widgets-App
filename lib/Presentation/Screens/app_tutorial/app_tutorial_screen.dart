55d108dbcbcf48f1e36b7500df8d4dfcba83ea22
TheLogan21 <miguel_dmartillo@outlook.com>
2025-03-04 11:54:10 -0500

    nuevas Screens creadas y archivo barril actualizado

---
+++ lib/Presentation/Screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutotrialScreen extends StatelessWidget {
+  static const name = "app_tutorial_screen";
+
+  const AppTutotrialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
